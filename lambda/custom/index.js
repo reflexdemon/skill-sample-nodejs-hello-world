ca1bb8381acba06541e6b82626465c5547a8d9dd
vprasanna <vprasanna@bridge2solutions.com>
2019-01-18 15:18:07 -0500

    Make the skill work

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,32 +3,33 @@
 
 const Alexa = require('ask-sdk-core');
 
-// const LaunchRequestHandler = require('./handlers/LaunchRequestHandler.handler');
-// const HelloWorldIntentHandler = require('./handlers/HelloWorldIntentHandler.handler');
-// const HelpIntentHandler = require('./handlers/HelpIntentHandler.handler');
-// const CancelAndStopIntentHandler = require('./handlers/CancelAndStopIntentHandler.handler');
-// const SessionEndedRequestHandler = require('./handlers/SessionEndedRequestHandler.handler');
+const LaunchRequestHandler = require('./handlers/LaunchRequestHandler.handler');
+const HelloWorldIntentHandler = require('./handlers/HelloWorldIntentHandler.handler');
+const HelpIntentHandler = require('./handlers/HelpIntentHandler.handler');
+const CancelAndStopIntentHandler = require('./handlers/CancelAndStopIntentHandler.handler');
+const SessionEndedRequestHandler = require('./handlers/SessionEndedRequestHandler.handler');
 const ErrorHandler = require('./handlers/ErrorHandler.handler');
+
+// const customHandlers = [
+//   './handlers/LaunchRequestHandler.handler',
+//   './handlers/HelloWorldIntentHandler.handler',
+//   './handlers/HelpIntentHandler.handler',
+//   './handlers/CancelAndStopIntentHandler.handler',
+//   './handlers/SessionEndedRequestHandler.handler'
+// ];
+
+// const customHandlerInstance = customHandlers.map((hdr) => require(hdr));
+
 const skillBuilder = Alexa.SkillBuilders.custom();
 
-const customHandlers = [
-  './handlers/LaunchRequestHandler.handler',
-  './handlers/HelloWorldIntentHandler.handler',
-  './handlers/HelpIntentHandler.handler',
-  './handlers/CancelAndStopIntentHandler.handler',
-  './handlers/SessionEndedRequestHandler.handler'
-];
-
-const customHandlerInstance = customHandlers.map((hdr) => require(hdr));
-
 exports.handler = skillBuilder
-  // .addRequestHandlers(
-  //   LaunchRequestHandler,
-  //   HelloWorldIntentHandler,
-  //   HelpIntentHandler,
-  //   CancelAndStopIntentHandler,
-  //   SessionEndedRequestHandler
-  // )
-  .addRequestHandlers(...customHandlerInstance)
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelloWorldIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  // .addRequestHandlers(...customHandlerInstance)
   .addErrorHandlers(ErrorHandler)
   .lambda();
