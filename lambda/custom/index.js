273972de31ed54d56c4fac9225a94463fdce1790
reflexdemon <venkateswara.vp@gmail.com>
2019-01-18 07:09:44 -0500

    Modularizing Lambda for cleaner structure

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,102 +3,32 @@
 
 const Alexa = require('ask-sdk-core');
 
-const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
-};
-
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'Hello World!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
-};
-
-const HelpIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
-};
-
-const CancelAndStopIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    const speechText = 'Goodbye!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
-};
-
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
-
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
-
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
-
-    return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
-};
-
+// const LaunchRequestHandler = require('./handlers/LaunchRequestHandler.handler');
+// const HelloWorldIntentHandler = require('./handlers/HelloWorldIntentHandler.handler');
+// const HelpIntentHandler = require('./handlers/HelpIntentHandler.handler');
+// const CancelAndStopIntentHandler = require('./handlers/CancelAndStopIntentHandler.handler');
+// const SessionEndedRequestHandler = require('./handlers/SessionEndedRequestHandler.handler');
+const ErrorHandler = require('./handlers/ErrorHandler.handler');
 const skillBuilder = Alexa.SkillBuilders.custom();
 
+const customHandlers = [
+  './handlers/LaunchRequestHandler.handler',
+  './handlers/HelloWorldIntentHandler.handler',
+  './handlers/HelpIntentHandler.handler',
+  './handlers/CancelAndStopIntentHandler.handler',
+  './handlers/SessionEndedRequestHandler.handler'
+];
+
+const customHandlerInstance = customHandlers.map((hdr) => require(hdr));
+
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    HelloWorldIntentHandler,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
-  )
+  // .addRequestHandlers(
+  //   LaunchRequestHandler,
+  //   HelloWorldIntentHandler,
+  //   HelpIntentHandler,
+  //   CancelAndStopIntentHandler,
+  //   SessionEndedRequestHandler
+  // )
+  .addRequestHandlers(...customHandlerInstance)
   .addErrorHandlers(ErrorHandler)
   .lambda();
